7fc3f355164ff4bbb9b237c084d9deacb8a6bf65
Ioana <ioana.buzelan@sap.com>
2017-07-24 11:22:21 +0300

    HelloWrold with server

--- HelloWorld/index.js
+++ HelloWorld/index.js
@@ -1,2 +1,29 @@
 /* Hello World! program in Node.js */
-console.log("Hello World!");
+"use strict";
+
+//required module
+var mHttp = require("http");
+
+//variables
+var iPort = 8081;
+var sMessage = "Hello World";
+
+//create Http server
+var oServer = mHttp.createServer(function(req, res){
+    console.log("Request received.");
+
+    res.writeHead(200, {'Content-Type' : 'text-plain'});
+    res.write(sMessage);
+    res.end();
+
+    console.log("Response sent!");
+});
+oServer.listen(iPort);
+
+// Console will print server info
+console.log('Server running at http://127.0.0.1:/' + iPort + '/');
+
+
+//sMessage = sMessage + "!";
+
+//console.log(sMessage);
