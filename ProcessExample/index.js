c2077aee93e3ff3c7b0c8e2ab13a3add374b7dc9
Ioana <ioana.buzelan@sap.com>
2017-07-25 10:25:16 +0300

    Timeout process, exit & beforeExit

---
+++ ProcessExample/index.js
@@ -0,0 +1,21 @@
+process.on('exit', function(code) {
+
+   // Following code will never execute.
+  // setTimeout(function() {
+  //    console.log("This will not run");
+  // }, 0);
+  
+   console.log('About to exit with code:', code);
+});
+
+process.on('beforeExit', function(code) {
+
+   // Following code will never execute.
+  setTimeout(function() {
+     console.log("This will not run");
+   }, 4000);
+  
+   console.log('About to beforeExit with code:', code);
+});
+
+console.log("Program Ended");
